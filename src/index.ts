632d9e639bfc3906b64e45fa2ea4b0bf7b630010
chylli-deriv <chylli@deriv.com>
2023-11-12 00:03:02 +0800

    done ok

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,115 @@
-// src/index.ts
-export function helloWorld(): string {
-    return 'Hello, world!';
+export default class Future<T> {
+    private _ready: boolean;
+    private _cancelled: boolean | null;
+    private _callbacks: Array<Function>;
+    private static _lastId: number = 0;    
+    private _id: number;
+    private _result: T | undefined;
+
+    constructor() {
+        this._ready = false;
+        this._callbacks = [];
+        this._id = Future._lastId++;
+        this._result = undefined;
+        this._cancelled = null;
+    }
+
+    get id(): number {
+        return this._id;
+    }
+
+    _new_with_subs(){
+        console.log("not implemented");
+    }    
+
+    wait_all() {
+        console.log("not implemented");
+    }
+    
+    needs_all() {
+        console.log("not implemented");
+    }
+
+    and_then() {
+        console.log("not implemented");
+    }
+    
+    or_else(){
+        console.log("not implemented");
+    }
+    
+    transform(){
+        console.log("not implemented");
+    }
+    
+    private _mark_ready(){
+        this._ready = true;
+        console.log("left part not implemented");
+    }
+
+    public done(result: T){
+        if(this.is_ready) {
+            throw new Error(`Future ${this.id} is already complete and cannot be done twice`);           
+        } 
+        if(this._result){
+            throw new Error(`Future ${this.id} is not a leaf Future, cannot be .done`);           
+        }
+        this._result = result;
+        this._mark_ready();
+    }
+
+    done_cb(){
+        console.log("not implemented");
+    }
+    
+    fail(){
+        console.log("not implemented");
+    }
+    
+    fail_cb(){
+        console.log("not implemented");
+    }
+
+    on_cancel(){
+        console.log("not implemented");
+    }
+
+    get is_cancelled(): boolean | null {
+        return this._cancelled;
+    }
+    
+    get is_ready(){
+        return this._ready;
+    }
+    
+    on_ready(){
+        console.log("not implemented");
+    }
+    
+    get(){
+        return this._result;
+        console.log("not implemented");
+    }
+    
+    on_done(){
+        console.log("not implemented");
+    }
+    
+    failure(){
+        console.log("not implemented");
+    }
+    
+    on_fail(){
+        console.log("not implemented");
+    }
+    
+    cancel(){
+        console.log("not implemented");
+    }
+    
+    cancel_cb(){
+        console.log("not implemented");
+    }
+    
+   
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,28 @@
-// test/index.test.ts
-import { helloWorld } from '../src/index';
+import Future from '../src/index';
 
-test('helloWorld function returns correct string', () => {
-    expect(helloWorld()).toBe('Hello, world!');});
+describe('Future', () => {
+    describe('constructor', () => {
+        it('should create a new Future instance', () => {
+            const future = new Future();
+            expect(future).toBeInstanceOf(Future);
+        });
+    });
+
+
+        let future: Future<number>;
+
+        beforeEach(() => {
+            future = new Future<number>();
+        });
+
+        it('should mark the future as ready when done is called', () => {
+            future.done(42);
+            expect(future.is_ready).toBe(true); // assuming you have an isReady method
+        });
+
+        it('should store the result when done is called', () => {
+            future.done(42);
+            expect(future.get()).toBe(42); // assuming you have a getResult method
+        });
+
+});
